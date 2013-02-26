0f4f23cb5d7c657e91824c26f8e47499cea1e0dd
michael <michaelrkn@gmail.com>
2013-02-26 14:11:54 -0800

    switch should_receive to stub when the stub is not part of the test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Word do
   context '#to_s' do
     it 'returns underscores the length of the word before any letters are guessed' do
-      Array.any_instance.should_receive(:sample).and_return('monkey')
+      Array.any_instance.stub(:sample).and_return('monkey')
       word = Word.new
       word.to_s.should eq '_ _ _ _ _ _'
     end
 
     it 'replaces the underscores with letters when they are guessed correctly' do
-      Array.any_instance.should_receive(:sample).and_return('palate')
+      Array.any_instance.stub(:sample).and_return('palate')
       word = Word.new
       word.guess('a')
       word.to_s.should eq '_ a _ a _ _'
@@ -18,13 +18,13 @@ describe Word do
 
   context '#guess' do
     it 'returns false if the letter is not in the word' do
-      Array.any_instance.should_receive(:sample).and_return('monkey')
+      Array.any_instance.stub(:sample).and_return('monkey')
       word = Word.new
       word.guess('z').should be_false
     end
 
     it 'returns true if the letter is in the word' do
-      Array.any_instance.should_receive(:sample).and_return('monkey')
+      Array.any_instance.stub(:sample).and_return('monkey')
       word = Word.new
       word.guess('m').should be_true
     end
@@ -32,7 +32,7 @@ describe Word do
 
   context '#number_of_letters' do
     it 'returns the number of letters in the word' do
-      Array.any_instance.should_receive(:sample).and_return('palate')
+      Array.any_instance.stub(:sample).and_return('palate')
       word = Word.new
       word.number_of_letters.should eq 5
     end
